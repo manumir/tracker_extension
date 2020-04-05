db6afd4df4226aa3615ad54a97eacecdace000da
manumir <enginner@tesla.com>
2020-04-05 23:39:25 +0100

    updated

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 var start;
 var map = new Map();
 var state;
-var flag=0;
+var url_in_flag=0;
 var lurl;
 
 chrome.alarms.create({periodInMinutes:1});
@@ -13,56 +13,60 @@ chrome.alarms.onAlarm.addListener(function() {
 });
 
 if (state != "minimized"){
-  console.log('this is executed' + state);
+  console.log('this is executed ' + state);
   chrome.tabs.onActivated.addListener(function() {
     chrome.tabs.query({'active': true, 'currentWindow': true},function(tabs) {
       for(url of map.keys()){
-        if(url == tabs[0].url && flag == 0){
-          flag = 1;
+        if(url == tabs[0].url && url_in_flag == 0){
+          url_in_flag = 1;
         }
       }
-      if (flag==0){   
+      if (url_in_flag==0){
         map.set(tabs[0].url,0);
       }
       if (start != "undefined" && lurl != 'undefined'){
-        map.set(String(lurl),String(Date.now()-start));
+        if (url_in_flag==1){
+          map.set(lurl,Number(map.get(lurl)) + (Date.now()-start)/1000);
+        }
+        else{
+          map.set(lurl,(Date.now()-start)/1000);
+        }
       };
       lurl=tabs[0].url;
       start = Date.now();
-      flag=0;
-      chrome.storage.sync.set({'urls':urls}, function() {
-        console.log('urls: ' + Array.from(map.keys()));
-      });
-      chrome.storage.sync.set({'times':times}, function() {
-        console.log('times: ' + Array.from(map.values()));
-      });
-      console.log(map);
+      url_in_flag=0;
     });
   });
 
   chrome.tabs.onUpdated.addListener(function(){
     chrome.tabs.query({'active': true, 'currentWindow': true}, function (tabs){
       for(url of map.keys()){
-        if(url == tabs[0].url && flag == 0){
-          flag = 1;
+        if(url == tabs[0].url && url_in_flag == 0){
+          url_in_flag = 1;
         }
       }
-      if (flag==0){   
+      if (url_in_flag==0){   
         map.set(tabs[0].url,0);
       }
       if (start != "undefined" && lurl != 'undefined'){
-        map.set(String(lurl),String(Date.now()-start));
+        if (url_in_flag==1){
+          map.set(lurl,Number(map.get(lurl)) + (Date.now()-start)/1000);
+        }
+        else{
+          map.set(lurl,(Date.now()-start)/1000);
+        }
       };
       lurl=tabs[0].url;
       start = Date.now();
-      flag=0;
-      chrome.storage.sync.set({'urls':urls}, function() {
-        console.log('urls: ' + Array.from(map.keys()));
-      });
-      chrome.storage.sync.set({'times':times}, function() {
-        console.log('times: ' + Array.from(map.values()));
-      });
-      console.log(map);
+      url_in_flag=0;
     });
   });
 }
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.msg == "send urls and times"){
+    map.delete(undefined);
+    sendResponse({urls: Array.from(map.keys()),
+                  times: Array.from(map.values())});
+  }
+});
--- popup.js
+++ popup.js
@@ -1,7 +1,11 @@
-chrome.storage.sync.get('urls', function(data) {
-	document.getElementById('urls').innerHTML="urls: "+data.urls;
-});
-
-chrome.storage.sync.get('times', function(data) {
-	document.getElementById('times').innerHTML= "time: "+data.times;
+chrome.runtime.sendMessage({msg: "send urls and times"}, function(response) {
+  //alert('urls: '+response.urls+'\ntimes: '+response.times);
+  for (var i = 0; i < response.urls.length; i++) {
+  	response.urls[i]
+  	var p = document.createElement("p");
+  	var text = document.createTextNode(response.urls[i]+': '+response.times[i]+' sec');
+  	p.appendChild(text);
+  	var element = document.getElementById("a");
+		element.appendChild(p);
+  }
 });
