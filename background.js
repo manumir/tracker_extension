54a715a346c410bb64660d064de222b3698e47cd
manumir <enginner@tesla.com>
2020-04-08 15:02:36 +0100

    something

--- background.js
+++ background.js
@@ -18,25 +18,13 @@ chrome.windows.onFocusChanged.addListener(function(windowId) {
 chrome.tabs.onActivated.addListener(function() {
   chrome.tabs.query({'active': true, 'currentWindow': true},function(tabs) {
     if (DONT_WRITE_NEXT_FLAG == 0){
-      for(url of map.keys()){
-        if(url == tabs[0].url && url_in_flag == 0){
-          url_in_flag = 1;
-        }
-      }
-      if (url_in_flag==0){
-        map.set(tabs[0].url,0);
-      }
-      if (start != "undefined" && lurl != 'undefined'){
-        if (url_in_flag==1){
-          map.set(lurl,Number(map.get(lurl)) + (Date.now()-start)/1000);
-        } else {
-          map.set(lurl,(Date.now()-start)/1000);
-        }
-      };
-    } 
-    else {
-      DONT_WRITE_NEXT_FLAG=0
+      write2map(tabs,map);
     }
+    else{
+      DONT_WRITE_NEXT_FLAG = 0;
+    }
+    console.log('0');
+    console.log(map);
     lurl=tabs[0].url;
     start = Date.now();
     url_in_flag=0;
@@ -45,22 +33,14 @@ chrome.tabs.onActivated.addListener(function() {
 
 chrome.tabs.onUpdated.addListener(function(){
   chrome.tabs.query({'active': true, 'currentWindow': true}, function (tabs){
-    for(url of map.keys()){
-      if(url == tabs[0].url && url_in_flag == 0){
-        url_in_flag = 1;
+    if (DONT_WRITE_NEXT_FLAG == 0){
+      write2map(tabs,map);
+      console.log('1');
+    } else {
+        DONT_WRITE_NEXT_FLAG = 0;
+        console.log('did not write');
       }
-    }
-    if (url_in_flag==0){   
-      map.set(tabs[0].url,0);
-    }
-    if (start != "undefined" && lurl != 'undefined'){
-      if (url_in_flag==1){
-        map.set(lurl,Number(map.get(lurl)) + (Date.now()-start)/1000);
-      }
-      else{
-        map.set(lurl,(Date.now()-start)/1000);
-      }
-    };
+    console.log(map);
     lurl=tabs[0].url;
     start = Date.now();
     url_in_flag=0;
@@ -70,7 +50,45 @@ chrome.tabs.onUpdated.addListener(function(){
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.msg == "send urls and times"){
     map.delete(undefined);
+    map=sortt(map);
+    console.log(map);
     sendResponse({urls: Array.from(map.keys()),
                   times: Array.from(map.values())});
   }
 });
+function write2map(tabs,map){
+  for(url of map.keys()){
+    if(url == tabs[0].url && url_in_flag == 0){
+      url_in_flag = 1;
+    }
+  }
+  if (url_in_flag==0){
+    map.set(tabs[0].url,0);
+  }
+  if (start != "undefined" && lurl != 'undefined'){
+    if (url_in_flag==1){
+      map.set(lurl,Number(map.get(lurl)) + (Date.now()-start)/1000);
+    } else {
+      map.set(lurl,(Date.now()-start)/1000);
+    }
+  };
+}
+
+function sortt(map){
+  var new_map = new Map();
+  var url_max;
+  var length=map.size;
+  var max=0;
+  for (var i =0; i<length;i++,max=-1){
+    for (url of map.keys()){
+      if (map.get(url) > max) {
+        max = map.get(url);
+        url_max = url;
+      }
+    }
+    new_map.set(url_max,map.get(url_max));
+    map.delete(url_max);
+    console.log('sorted');
+  }
+  return new_map;
+}
