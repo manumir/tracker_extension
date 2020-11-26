84e07db8dadc8e7bd004f873ff9f4baa64067d74
manumir <49996755+manumir@users.noreply.github.com>
2020-11-26 14:41:04 +0000

    Add files via upload

--- background.js
+++ background.js
@@ -1,9 +1,40 @@
-var start;
+var state, lurl;
 var map = new Map();
-var state;
-var url_in_flag=0;
 var DONT_WRITE_NEXT_FLAG=0;
-var lurl;
+
+setInterval(function () {
+  if (lurl != undefined && DONT_WRITE_NEXT_FLAG == 0){
+    if (map.get(lurl) != undefined){
+      map.set(lurl,Number(map.get(lurl)+1));
+    }
+    else {
+      map.set(lurl,1);
+    }
+  }
+}, 1000);
+
+chrome.tabs.onActivated.addListener(function() {
+  chrome.tabs.query({'active': true, 'currentWindow': true},function(tabs) {
+    lurl=getRootUrl(tabs[0].url);
+  });
+});
+
+chrome.tabs.onUpdated.addListener(function(tabid,props,tab){
+  //if (props.status == 'complete'){
+  if (tab.url != undefined){
+    lurl=getRootUrl(tab.url) // or = props.url (both have a .url attribute)
+  }
+  //}
+});
+
+// send map to popup
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.msg == "send urls and times"){
+    //map1=sortt(map);
+    sendResponse({urls: Array.from(map.keys()),
+                  times: Array.from(map.values())});
+  }
+});
 
 chrome.windows.onFocusChanged.addListener(function(windowId) {
   chrome.windows.getCurrent(function(_window){
@@ -12,86 +43,27 @@ chrome.windows.onFocusChanged.addListener(function(windowId) {
     if (state == 'minimized'){
       DONT_WRITE_NEXT_FLAG = 1;
     }
-  });
-});
-
-chrome.tabs.onActivated.addListener(function() {
-  chrome.tabs.query({'active': true, 'currentWindow': true},function(tabs) {
-    if (DONT_WRITE_NEXT_FLAG == 0){
-      write2map(tabs,map);
-    }
-    else{
+    if (state != 'minimized'){
       DONT_WRITE_NEXT_FLAG = 0;
     }
-    console.log('0');
-    console.log(map);
-    lurl=tabs[0].url;
-    start = Date.now();
-    url_in_flag=0;
   });
 });
 
-chrome.tabs.onUpdated.addListener(function(tabID,props){
-  //if (props.status == 'complete'){
-    chrome.tabs.query({'active': true, 'currentWindow': true}, function (tabs){
-      if (DONT_WRITE_NEXT_FLAG == 0){
-        write2map(tabs,map);
-        console.log('1');
-      } else {
-          DONT_WRITE_NEXT_FLAG = 0;
-          console.log('did not write');
-        }
-      console.log(map);
-      lurl=tabs[0].url;
-      start = Date.now();
-      url_in_flag=0;
-    });
-  //}
+// get all tabs urls at browser startup
+chrome.tabs.query({'currentWindow': true},function(tabs) {
+  for (var i = tabs.length - 1; i >= 0; i--) {
+    tabs[i].url=getRootUrl(tabs[i].url);
+    if (tabs[i].url != undefined){
+      map.set(tabs[i].url,0);
+    }
+  }
+  console.log(map);
 });
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.msg == "send urls and times"){
-    map.delete(undefined);
-    map=sortt(map);
-    console.log(map);
-    sendResponse({urls: Array.from(map.keys()),
-                  times: Array.from(map.values())});
-  }
+chrome.tabs.query({'active': true, 'currentWindow': true},function(tabs) {
+  lurl=getRootUrl(tabs[0].url);
 });
 
-function write2map(tabs,map){
-  for(url of map.keys()){
-    if(url == tabs[0].url && url_in_flag == 0){
-      url_in_flag = 1;
-    }
-  }
-  if (url_in_flag==0){
-    map.set(tabs[0].url,0);
-  }
-  if (start != "undefined" && lurl != 'undefined'){
-    if (url_in_flag==1){
-      map.set(lurl,Number(map.get(lurl)) + (Date.now()-start)/1000);
-    } else {
-      map.set(lurl,(Date.now()-start)/1000);
-    }
-  };
-}
-
-function sortt(map){
-  var new_map = new Map();
-  var url_max;
-  var length=map.size;
-  var max=0;
-  for (var i =0; i<length;i++,max=-1){
-    for (url of map.keys()){
-      if (map.get(url) > max) {
-        max = map.get(url);
-        url_max = url;
-      }
-    }
-    new_map.set(url_max,map.get(url_max));
-    map.delete(url_max);
-    console.log('sorted');
-  }
-  return new_map;
+function getRootUrl(url) {
+  return url.toString().replace(/^(.*\/\/[^\/?#]*).*$/,"$1");
 }
--- popup.js
+++ popup.js
@@ -1,10 +1,21 @@
+setInterval(function () {
 chrome.runtime.sendMessage({msg: "send urls and times"}, function(response) {
   for (var i = 0; i < response.urls.length; i++) {
-  	response.urls[i]
-  	var p = document.createElement("p");
-  	var text = document.createTextNode(response.urls[i]+': '+response.times[i]+' sec');
-  	p.appendChild(text);
-  	var element = document.getElementById("a");
-		element.appendChild(p);
+	if (response.urls[i] != undefined){
+		if (document.getElementById(response.urls[i]) == null){
+		  	var p = document.createElement("p");
+		  	p.setAttribute("id",response.urls[i]);
+		  	
+		  	//p.appendChild(document.createTextNode(response.urls[i]+': '+response.times[i]+' sec'));
+		  	p.textContent= response.urls[i]+': '+String(Number(response.times[i])/60)+' min';
+		  	var element = document.getElementById("a");
+			element.appendChild(p);
+		  	}
+		  	else {
+		  		var p = document.getElementById(response.urls[i]);
+		  		p.textContent = response.urls[i]+': '+String(Number(response.times[i])/60)+' min';
+	  	}
+	}
   }
 });
+}, 1000);
