1acbb22a1ea281c217d03aefd280c7b81dec1eda
manumir <49996755+manumir@users.noreply.github.com>
2020-11-26 21:37:44 +0000

    Add files via upload

--- background.js
+++ background.js
@@ -30,9 +30,9 @@ chrome.tabs.onUpdated.addListener(function(tabid,props,tab){
 // send map to popup
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.msg == "send urls and times"){
-    //map1=sortt(map);
-    sendResponse({urls: Array.from(map.keys()),
-                  times: Array.from(map.values())});
+    map1=sortt(map);
+    sendResponse({urls: Array.from(map1.keys()),
+                  times: Array.from(map1.values())});
   }
 });
 
@@ -47,6 +47,17 @@ chrome.windows.onFocusChanged.addListener(function(windowId) {
       DONT_WRITE_NEXT_FLAG = 0;
     }
   });
+});
+
+chrome.idle.setDetectionInterval(300);
+chrome.idle.onStateChanged.addListener(function (new_state){
+  if (new_state != "active"){
+    DONT_WRITE_NEXT_FLAG = 1;
+    console.log("not active");
+  }
+  else{
+    DONT_WRITE_NEXT_FLAG = 0;
+  }
 });
 
 // get all tabs urls at browser startup
@@ -66,4 +77,28 @@ chrome.tabs.query({'active': true, 'currentWindow': true},function(tabs) {
 
 function getRootUrl(url) {
   return url.toString().replace(/^(.*\/\/[^\/?#]*).*$/,"$1");
+}
+
+function sortt(map_arg){
+  var new_map = new Map();
+  var url_max;
+  var n=map_arg.size;
+  var max=0;
+  var keys_=Array.from(map_arg.keys());
+
+  for (var i = 0; i<n; i++,max=-1){
+    for (url of keys_){
+      if (map_arg.get(url) > max) {
+        max = map_arg.get(url);
+        url_max = url;
+      }
+    }
+    new_map.set(url_max,map_arg.get(url_max));
+    for (var j = keys_.length - 1; j >= 0; j--) {
+      if (keys_[j] == url_max){
+        keys_.splice(j,1);
+      }
+    }
+  }
+  return new_map;
 }
--- popup.js
+++ popup.js
@@ -1,19 +1,20 @@
 setInterval(function () {
 chrome.runtime.sendMessage({msg: "send urls and times"}, function(response) {
   for (var i = 0; i < response.urls.length; i++) {
-	if (response.urls[i] != undefined){
+	if (response.urls[i] != (undefined || "")){
+		console.log(response.urls[i],response.times[i]);
 		if (document.getElementById(response.urls[i]) == null){
 		  	var p = document.createElement("p");
 		  	p.setAttribute("id",response.urls[i]);
 		  	
 		  	//p.appendChild(document.createTextNode(response.urls[i]+': '+response.times[i]+' sec'));
-		  	p.textContent= response.urls[i]+': '+String(Number(response.times[i])/60)+' min';
+		  	p.textContent= response.urls[i]+': '+String(Number(response.times[i]))+' sec';
 		  	var element = document.getElementById("a");
 			element.appendChild(p);
 		  	}
 		  	else {
 		  		var p = document.getElementById(response.urls[i]);
-		  		p.textContent = response.urls[i]+': '+String(Number(response.times[i])/60)+' min';
+		  		p.textContent = response.urls[i]+': '+String(Number(response.times[i]))+' sec';
 	  	}
 	}
   }
